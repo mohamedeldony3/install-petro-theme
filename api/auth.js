93580d994bbcfd0a4c82a999ac7f4c109c714dca
mohamedeldony3 <60990149+mohamedeldony3@users.noreply.github.com>
2025-11-05 22:49:16 +0200

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -5,8 +5,8 @@ export default function handler(req, res) {
   const auth = req.headers.authorization;
 
   // اسم المستخدم وكلمة المرور (عدّلهم زي ما تحب)
-  const username = "admin";
-  const password = "12345";
+  const username = "elsony";
+  const password = "010614";
 
   // لو مفيش مصادقة، نرجع طلب تسجيل الدخول
   if (!auth) {
